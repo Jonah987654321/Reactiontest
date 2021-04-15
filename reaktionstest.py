43f93d7488329045f67c40087203c2b3def284e2
Pythonwolf7734 <82569523+Pythonwolf7734@users.noreply.github.com>
2021-04-15 10:02:57 +0200

    Updating language from german to english

--- reaktionstest.py
+++ reaktionstest.py
@@ -8,7 +8,7 @@ def test():
     welcome_message.destroy()
     free.destroy()
     free2.destroy()
-    instruction = Text(app, text="Drücke auf den roten Kreis sobald er erscheint.")
+    instruction = Text(app, text="Push the red circle as soon as he appears")
     instruction.text_color = "white"
     board = Waffle(app, height=5, width=5, visible=True)
     board.pixel_size = 75
@@ -24,12 +24,9 @@ def test():
             board.set_pixel(x, y, "white")
             end = time.perf_counter()
             timeused = round((end - starttime) * 1000) / 1000
-            zeit = Text(app, text="Deine Zeit: " + str(timeused) + "s")
-            zeit.text_color = "white"
-            zeit.font = "Impact"
-            # restart = PushButton(app, restart, text="Neustart")
-            # restart.bg = "#a3a3a3"
-            # restart.text_color = "white"
+            timedisplay = Text(app, text="Your reaction-time: " + str(timeused) + "s")
+            timedisplay.text_color = "white"
+            timedisplay.font = "Impact"
 
     board.update_command(ende)
     x, y = random.randint(0, 4), random.randint(0, 4)
@@ -40,7 +37,7 @@ def test():
 app = App(title="Reaktionstest")
 app.bg = "black"
 free = Text(app, text="\n\n\n\n\n\n")
-welcome_message = Text(app, text="Wilkommen beim Reaktionstest.\n Hier kannst du deine Reaktion testen.")
+welcome_message = Text(app, text="Welcome to the reaction test. \nHere you can test your reaction.")
 welcome_message.text_color = "white"
 welcome_message.font = "Impact"
 free2 = Text(app, text="\n")
